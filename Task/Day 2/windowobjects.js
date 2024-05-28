b91ab4c19c5bc7dd8384f5e839737b657e95fe1f
Easwaramurth <easwaramurthi14.cs@gmail.com>
2024-05-28 17:41:48 +0530

    Add files via upload

---
+++ Task/Day 2/windowobjects.js
@@ -0,0 +1,8 @@
+This blog explores the differences between Document and Window object in JavaScript, focusing on their roles, scopes, and functionalities, as they are essential components of the Document Object Model (DOM) for efficient web development.
+
+Window Object
+The Window object is a global object in client-side JavaScript, representing the browser window containing a DOM document and acting as the root of the document object model.
+
+The window object, supported by all browsers, represents the browser’s window and automatically includes global JavaScript objects, functions, and variables as members.
+
+The Window object is responsible for managing global variables, functions, and objects, providing methods for browser interaction and managing properties related to frames, tabs, or windows, such as alert(), confirm(), setTimeout(), and setInterval().
