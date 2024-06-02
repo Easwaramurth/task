a777295c218fd4229ffe4f6085848b0040e3e6c5
Easwaramurth <easwaramurthi14.cs@gmail.com>
2024-06-02 20:27:39 +0530

    Add files via upload

---
+++ Task/Day 5/resume.js
@@ -0,0 +1,52 @@
+let myResume={
+    "basics": {
+      "name": "easwaramurthi S",
+      "email": "easwaramurthi14.cs@gmail.com",
+      "phone": 9715370796,
+      "degree": "BSC.CS",
+      "location": {
+        "address": "1/62 kallapuram ellur PO",
+        "postalCode": 641032,
+        "city": "Coimbatore",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/Easwaramurthi"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Indian product privated limtted",
+        "position": "Executive",
+        "startDate": "2017-11-01",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Rathinam College Of Arts & Science",
+        "department": "Computer Scicence",
+        "studyType": "fulltime",
+        "batch start year": 2014,
+        "batch end year": 2017,
+        "gpa": 6.2,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+        
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish,Kannada",
+      }
+    ],
+    
+  }
+  console.log(myResume);
+  
